4311e6b4a0b29ba89bd3a25ba0315663fcc0efe2
Presh-Codes <110492316+Presh-Codes@users.noreply.github.com>
2022-08-02 12:56:41 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My First Php</title>
+    </head>
+    <body>
+        <?php
+            $x = 3.4;
+            echo (int)$x;             
+        ?>
+    </body>
+</html>
